546643f304fb92b0b7ddd7f9928f1995b1d606a6
Keith <germanbohler@gmail.com>
2024-03-09 19:55:18 -0600

    Moved quadratic equation logic to its own function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::io;
 use std::f32;
+use std::process;
+
 struct QuadraticCoEff {
     a: f32,
     b: f32,
     c: f32,
 }
+
+struct AnsHolder {
+    d: f32,
+    r1: f32,
+    r2: f32,
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -32,22 +41,37 @@ fn main() {
     quad1.c = line_input.trim().parse().expect("Input not an integer!");
     line_input.clear();
 
+    let equ_solustion = quad_calc(quad1);
 
-    let quadradic_discriminant = (quad1.b * quad1.b) - 4.0 * quad1.a * quad1.c;
-
-
-    if quadradic_discriminant < 0.0 {
-        println!("Solution is imaginary. ");
-    } else if quadradic_discriminant == 0.0 {
-        let sol1 = (quad1.b + quadradic_discriminant.sqrt()) / (2.0 * quad1.a);
-        println!("There is one real solution: {}", sol1);
+    if equ_solustion.d == 0.0 {
+        println!("There is one real solution: {}", equ_solustion.r1);
     } else {
-        let sol1 = (quad1.b + quadradic_discriminant.sqrt()) / (2.0 * quad1.a);
-        let sol2 = (quad1.b - quadradic_discriminant.sqrt()) / (2.0 * quad1.a);
-        println!("There are two solutions. Positive root: {} and negative root: {}", sol1, sol2);
+        println!("There are two solutions. Positive root: {} and negative root: {}", equ_solustion.r1, equ_solustion.r2);
     }
 
     println!("Fin");
 
 
+}
+
+fn quad_calc(coeffs: QuadraticCoEff) -> AnsHolder{
+    // 
+
+    let quadradic_discriminant = (coeffs.b * coeffs.b) - 4.0 * coeffs.a * coeffs.c;
+
+    if quadradic_discriminant < 0.0 {
+        println!("Solution is imaginary. ");
+        process::exit(0);
+    }
+
+    let pos_root = (coeffs.b + quadradic_discriminant.sqrt()) / (2.0 * coeffs.a);
+    let neg_root = (coeffs.b - quadradic_discriminant.sqrt()) / (2.0 * coeffs.a);
+
+    let sol = AnsHolder{
+        d: quadradic_discriminant,
+        r1: pos_root,
+        r2: neg_root,
+    };
+
+    return sol;
 }
